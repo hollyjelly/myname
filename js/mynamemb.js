69169102ab995976c3b7ed1779f42ac0de1cb2f0
hollyjelly <skguswl8038@naver.com>
2022-05-20 15:09:04 +0900

    js commit

---
+++ js/mynamemb.js
@@ -0,0 +1,31 @@
+$(function(){
+    var group = $('.main.main02 .infoBtm .group');
+    var ale = $('.main.main02 .infoBtm .alert');
+    var aleimg = $('.main.main02 .infoBtm .alert .image');
+    var xx = $('.main.main02 .infoBtm .alert span');
+    var chip = $('.main.main03 .chip .image');
+    var view = $('.main.main03 .game .view');
+    var img = 0,ind = 0, pt = 0;
+
+    
+    function alertcli(){
+        ind = $(this).index();
+        pt = $(this).attr('data-pt');
+        ale.css({zIndex:'5'});
+        aleimg.css({backgroundImage:pt});
+    }
+    
+    function chipclick(){
+        img = $(this).attr('data-img');
+        view.css({backgroundImage:img});
+        view.toggleClass('on');
+    }
+    
+    function xcli(){
+        ale.css({zIndex:'-1'});
+    }
+    
+    xx.click(xcli);
+    group.click(alertcli);
+    chip.click(chipclick);
+});
